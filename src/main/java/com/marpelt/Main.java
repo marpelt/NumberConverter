67d9c80a1c6090ee7cbbcd8258a0aa328a5e17aa
Markus Peltsarszky <149835564+marpelt@users.noreply.github.com>
2023-12-21 23:25:09 +0100

    Delete src/main directory

--- src/main/java/com/marpelt/Main.java
+++
@@ -1,9 +0,0 @@
-package com.marpelt;
-import javax.swing.*;
-
-public class Main extends JFrame {
-
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(Window::new);
-    }
-}
